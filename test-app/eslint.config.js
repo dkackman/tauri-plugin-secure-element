08abe35d469d0936385668db840e14905d10bd44
Don Kackman <dkackman@gmail.com>
2025-12-07 12:27:15 -0600

    update eslint

---
+++ test-app/eslint.config.js
@@ -0,0 +1,56 @@
+import js from "@eslint/js";
+import svelte from "eslint-plugin-svelte";
+import svelteParser from "svelte-eslint-parser";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  js.configs.recommended,
+  ...svelte.configs["flat/recommended"],
+  {
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+      "build/**",
+      "target/**",
+      "src-tauri/**",
+      "*.config.js",
+      "vite.config.js",
+      ".eslintrc.cjs",
+    ],
+  },
+  {
+    languageOptions: {
+      parser: svelteParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+      parserOptions: {
+        parser: {
+          js: "espree",
+          ts: tsParser,
+        },
+      },
+    },
+    files: ["**/*.svelte"],
+    plugins: {
+      svelte,
+    },
+    rules: {},
+  },
+  {
+    files: ["**/*.js", "**/*.mjs", "**/*.cjs"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
+    rules: {},
+  },
+];
